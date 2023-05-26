0d9cd51a952d2bdecf729d68b1236c876b429c61
kolown <kolown@gmail.com>
2023-05-26 20:08:00 +0800

    added index and webgpu.js

---
+++ webgpu.js
@@ -0,0 +1,37 @@
+ const canvas = document.querySelector("canvas");
+   
+   // WebGPU device initialization
+   if (!navigator.gpu) {
+    throw new Error("WebGPU not supported on this browser.");
+  }
+
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) {
+    throw new Error("No appropriate GPUAdapter found.");
+  }
+
+  const device = await adapter.requestDevice();
+
+  // Canvas configuration
+  const context = canvas.getContext("webgpu");
+  const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+  context.configure({
+      device: device,
+      format: canvasFormat,
+  });
+
+  // Clear the canvas with a render pass
+  const encoder = device.createCommandEncoder();
+
+  const pass = encoder.beginRenderPass({
+      colorAttachments: [{
+              view: context.getCurrentTexture().createView(),
+              loadOp: "clear",
+              clearValue: { r: 0, g: 0, b: 0.4, a: 1.0 },
+              storeOp: "store",
+          }]
+  });
+
+  pass.end();
+
+  device.queue.submit([encoder.finish()]);
